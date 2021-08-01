546e5b373e9546cfecabbca89631ea6fdd7d0552
Sebastian Dopazo <sebastiandopazo@outlook.com.ar>
2021-08-01 00:54:07 -0300

    home

--- src/components/categoria/infillcategoria.jsx
+++ src/components/categoria/infillcategoria.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import BotonModi from '../utility/botonmodificar';
 import Boton from '../utility/boton';
 import handleDelete from '../../middleware/delete';
-import { categoria, urlroot, port} from '../../urls';
 
 export default function InfillCategoria(props) {
     var infill = <><p class="whites">{props.categoria.nombre}</p>
